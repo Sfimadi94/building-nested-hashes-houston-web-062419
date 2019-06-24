5c3609994dd3ffbd136b0d775d4028a73a2c1b23
Syed Imadi <sydeffect1994@gmail.com>
2019-06-24 07:30:14 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,8 +1,8 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      patriarch 
       #your key/value pairs here!
+      
       }, 
     :capulet => {
       #your key/value pairs here!
